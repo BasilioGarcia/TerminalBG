2ce52076ccd393e8b143036645dd2b4091e6a268
BasilioGarcia <basiliog80@gmail.com>
2024-06-09 21:01:05 +0200

    primeros ejemplos

--- docs/js/examples.js
+++ docs/js/examples.js
@@ -5,6 +5,33 @@ export function examples() {
 }
 
 function buttonClick(event) {
-    const button = event.target;
-    console.log(button.getAttribute('data-example'));
+    const name = event.target.getAttribute('data-example');
+    const dispatcher = {
+        'color': exampleColor,
+        'bcolor': exampleBcolor,
+        'clear': exampleClear,
+        'size': exampleSize,
+    };
+
+    const exampleFunction = dispatcher[name];
+
+    if (exampleFunction) {
+        exampleFunction();
+    }
+}
+
+function exampleColor () {
+    tBG('texto de salida').color("#FF0000")._();
+}
+
+function exampleBcolor () {
+    tBG('texto de salida').bcolor("blue")._();
+}
+
+function exampleClear () {
+    tBG().clear();
+}
+
+function exampleSize () {
+    tBG('texto de salida').size("10rem")._();
 }
