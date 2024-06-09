08aacece6e4b442f80a44ecb9f948680e7f33f58
BasilioGarcia <basiliog80@gmail.com>
2024-06-09 12:59:29 +0200

    minor fix

--- docs/js/main.js
+++ docs/js/main.js
@@ -2,6 +2,5 @@ import { examples } from "./examples.js";
 import { terminalBG } from "../../terminal-bg-module.js";
 
 document.addEventListener("DOMContentLoaded", function() {
-    //example();
-    terminalBG();
+    examples();
 });
